1871ce2751ba9bfe2e86d49dfb0f054b5ec1e0e8
bogdan_kozintsov <bogdan.kozintsov@gmail.com>
2023-12-26 16:13:41 +1100

    added summator func

--- utils.py
+++ utils.py
@@ -2,3 +2,6 @@ print("Hello from local(New Conflict!!!!)")
 print("Hello from remote")
 print("Hello from local(New Conflict)")
 print("Hello from remote(new conflict!!!!!")
+
+def suumator(a, b):
+    return a + b
